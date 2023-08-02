8880c48ece7e0a463a8b500e4af5840dd2d26734
Esteban <estebanvargas10@gmail.com>
2023-08-01 21:16:03 -0600

    Changes

---
+++ vcrpage/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Vehículos Costa Rica</h1>
+        <h4>2024</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
